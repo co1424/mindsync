065f0c712c938f51ca6ac012bb2b3437282ec2e9
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-11-22 20:41:08 -0700

    document list

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,6 +1,13 @@
-"use client"
-import React from 'react'
-import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus, Trash } from "lucide-react";
+'use client';
+import React from 'react';
+import {
+  ChevronDown,
+  ChevronRight,
+  LucideIcon,
+  MoreHorizontal,
+  Plus,
+  Trash,
+} from 'lucide-react';
 import { Id } from '@/convex/_generated/dataModel';
 import { cn } from '@/lib/utils';
 import {
@@ -8,11 +15,17 @@ import {
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSeparator
-} from "@/components/ui/dropdown-menu";
+  DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu';
+import { Skeleton } from '@/components/ui/skeleton';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { on } from 'events';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 interface ItemProps {
-  id?: Id<"documents">;
+  id?: Id<'documents'>;
   documentIcon?: string;
   active?: boolean;
   expanded?: boolean;
@@ -22,72 +35,112 @@ interface ItemProps {
   label: string;
   onClick: () => void;
   icon: LucideIcon;
-};
+}
 
-const Item = ({
-    label, 
-    onClick,
-    icon: Icon,
-    active,
-    documentIcon,
-    isSearch,
-    level = 0,
-    onExpand,
-    expanded,
+export const Item = ({
+  id,
+  label,
+  onClick,
+  icon: Icon,
+  active,
+  documentIcon,
+  isSearch,
+  level = 0,
+  onExpand,
+  expanded,
 }: ItemProps) => {
+  const create = useMutation(api.documents.create);
+  const router = useRouter();
+
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    onExpand?.();
+  };
+
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    if (!id) return;
+
+    const promise = create({ title: 'untitled', parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+        // router.push(`/documents/${documentId}`);
+      }
+    );
+    toast.promise(promise, {
+      loading: 'Creating a new Note...',
+      success: 'Note not created yet!',
+      error: 'Something went wrong while creating a new Note!',
+    });
+  };
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
-
-
-
 
   return (
     <div
-    onClick={onClick}
-    role="button"
-    style={{ paddingLeft: level ? `${(level * 12)}px` : "12px" 
-  }}
-    className={cn(
-      "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
-      active && "bg-primary/5 text-primary"
+      onClick={onClick}
+      role="button"
+      style={{ paddingLeft: level ? `${level * 12}px` : '12px' }}
+      className={cn(
+        'group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium',
+        active && 'bg-primary/5 text-primary'
       )}
     >
-
-      
-      {/* esta parte causa error por que no entiende que el type de id esta en el interface de arriba chat gpt dice que va a entender una ves lo usemos por eso comente por mientras el bloque de codigo */}
-
-      {/* {!!id && (
+      {!!id && (
         <div
-        role="button"
-        className='h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1'
-        onClick={() => {}}
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+          onClick={handleExpand}
         >
-          <ChevronIcon
-          className='h-4 w-4 shrink-0 text-muted-foreground/50'
-          />
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
-      )} */}
+      )}
 
       {documentIcon ? (
-        <div className='shrink-0 h-[18px] mr-2 text-muted-foreground'>
+        <div className="shrink-0 h-[18px] mr-2 text-muted-foreground">
           {documentIcon}
         </div>
       ) : (
-        
-        <Icon className="shrink-0 h-[18px] mr-2
-        text-muted-foreground" 
+        <Icon
+          className="shrink-0 h-[18px] mr-2
+        text-muted-foreground"
         />
-        )}
-        <span className="truncate">
-        {label}
-        </span>
-        {isSearch && (
-          <kbd className='ml-auto pointer-events-none inline-flex h-5 select-none items-center gar-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100 '>
-            <span className='text-xs'>⌘</span>K
-          </kbd>
-        )
-        }
+      )}
+      <span className="truncate">{label}</span>
+      {isSearch && (
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gar-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100 ">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={onCreate}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Item
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${(level * 12) + 25}px` : "12px"
+      }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
+    </div>
+  );
+};
