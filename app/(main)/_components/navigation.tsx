c76d12d2051dc97ebf63f3dda3655fd47a9f4a70
Eddy A. Renteria <90665395+co1424@users.noreply.github.com>
2023-12-05 20:47:11 -0700

    editing feature

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -9,7 +9,7 @@ import {
   Settings,
   Trash
 } from "lucide-react";
-import { useParams, usePathname } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { useMutation } from "convex/react";
@@ -33,6 +33,7 @@ import { Navbar } from "./navbar";
 import { SearchCommand } from "@/components/search-command";
 
 export const Navigation = () => {
+  const router = useRouter();
   const settings = useSettings()
   const search = useSearch()
   const params = useParams();
@@ -122,7 +123,8 @@ export const Navigation = () => {
   }
 
   const handleCreate = () => {
-    const promise = create({ title: "Untitled" });
+    const promise = create({ title: "Untitled" })
+      .then((documentId) => router.push(`/documents/${documentId}`))
 
     toast.promise(promise, {
       loading: "Creating a new note...",
